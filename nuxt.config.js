1f0662cae0f202abd047f7959044d2719718ab18
VALTECH-COM\miguel.minoldo <miguel.minoldo@valtech.com>
2021-10-11 08:00:56 +0200

    Updated the worflow file

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,6 +52,9 @@ export default {
       }
     }
   },
+  generate: {
+    routes: ['/home/{110D559F-DEA5-42EA-9C1C-8A5DF7E70EF9}']
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
